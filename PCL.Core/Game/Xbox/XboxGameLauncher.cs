449f7e8b078d86c8a00da4c25ab4dd08e724df6e
LuLu-ling <LuLu-ling@outlook.com>
2026-05-09 23:47:37 +0800

    fix

--- PCL.Core/Game/Xbox/XboxGameLauncher.cs
+++ PCL.Core/Game/Xbox/XboxGameLauncher.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-using System.IO;
 
 namespace PCL.Core.Game.Xbox;
 
@@ -7,11 +6,31 @@ public static class XboxGameLauncher
 {
     public static void Launch(string gameId)
     {
+        var publisherId = GetPublisherId(gameId);
         Process.Start(new ProcessStartInfo
         {
             FileName = "explorer.exe",
-            Arguments = $"shell:AppsFolder\\{gameId}!Game",
+            Arguments = $"shell:AppsFolder\\{gameId}_{publisherId}!Game",
             UseShellExecute = false
         });
+    }
+
+    private static string? GetPublisherId(string gameId)
+    {
+        try
+        {
+            using var process = new Process
+            {
+                StartInfo = new ProcessStartInfo("powershell.exe", $"-NoProfile -Command \"(Get-AppxPackage -Name '{gameId}').PublisherId\"")
+                {
+                    UseShellExecute = false, RedirectStandardOutput = true, CreateNoWindow = true
+                }
+            };
+            process.Start();
+            var output = process.StandardOutput.ReadToEnd().Trim();
+            process.WaitForExit(5000);
+            return string.IsNullOrEmpty(output) ? null : output;
+        }
+        catch { return null; }
     }
 }
